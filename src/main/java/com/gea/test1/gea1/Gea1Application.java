b764562c5f2ca47e1df96aee73323d77399c9f86
juan sanchez <johhns@hotmail.com>
2023-02-08 08:17:21 -0600

    primer commit

---
+++ src/main/java/com/gea/test1/gea1/Gea1Application.java
@@ -0,0 +1,13 @@
+package com.gea.test1.gea1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gea1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gea1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gea/test1/gea1/Gea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gea.test1.gea1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
